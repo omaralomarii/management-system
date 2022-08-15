c886001c9673a7cc45776c991f4f5e1d274be79d
omaralomarii <omar.alomarii911@gmail.com>
2022-08-15 19:13:05 +0300

    add dom

--- app.js
+++ app.js
@@ -53,14 +53,54 @@ for( let i=0;i<employees.length;i++){
 
 
 
+//\\\\\\\\\\\\\\\\\\\\
 
 
 
+let main = document.querySelector("main")
 
+const eTable = document.createElement('table');
+main.appendChild(eTable)
 
+const tHeader = document.createElement('thead');
+eTable.appendChild(tHeader);
 
+const firstData = document.createElement("th");
+tHeader.appendChild(firstData);
+firstData.textContent="ID"
 
+const secondData = document.createElement("th");
+tHeader.appendChild(secondData);
+secondData.textContent="FullName"
 
+const thirdData = document.createElement("th");
+tHeader.appendChild(thirdData);
+thirdData.textContent="Department"
+
+const fourthData = document.createElement("th");
+tHeader.appendChild(fourthData);
+fourthData.textContent="Level"
+
+const fifthData = document.createElement("th");
+tHeader.appendChild(fifthData);
+fifthData.textContent="Salary"
+
+//   Table header ends here
+
+const arr = ['id','fullname','dep','level','salary']
+
+for (let i = 0; i < employees.length ; i++) {
+
+    const employeeRow = document.createElement('tr');
+    eTable.appendChild(employeeRow);
+
+    for (let c = 0; c < arr.length; c++) {
+        const cell = document.createElement('td')
+        employeeRow.appendChild(cell)
+        cell.textContent= employees[i][arr[c]]
+    }
+
+}
 
 
 
